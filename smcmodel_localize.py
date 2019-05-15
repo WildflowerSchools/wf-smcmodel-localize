f5b077690720456d994eb0fc2bc16985836848b0
Theodore Quinn <tcquinn@tcquinn.org>
2019-05-15 12:44:14 -0500

    Add ability to handle fixed object dimensions

--- smcmodel_localize.py
+++ smcmodel_localize.py
@@ -6,7 +6,9 @@ import tensorflow_probability as tfp
 def localization_model(
     num_objects = 3,
     num_anchors = 4,
-    num_dimensions = 2,
+    num_moving_object_dimensions = 2,
+    num_fixed_object_dimensions = 0,
+    fixed_object_positions = None,
     room_corners = [[0.0, 0.0], [10.0, 20.0]],
     anchor_positions = [
         [0.0, 0.0],
@@ -21,15 +23,52 @@ def localization_model(
     mean_rssi_slope = -20.0,
     rssi_std_dev = 5.0,
     ping_success_rate = 1.0):
-
+    if fixed_object_positions is not None and num_fixed_object_dimensions == 0:
+        raise ValueError('If fixed_object_positions argument is present, num_fixed_object_dimensions argument must be > 0')
+    if fixed_object_positions is None and num_fixed_object_dimensions != 0:
+        raise ValueError('If num_fixed_object_dimensions argument > 0, fixed_object_positions argument must be present')
+    if fixed_object_positions is not None:
+        fixed_object_positions = np.asarray(fixed_object_positions)
+        if fixed_object_positions.size != num_objects * num_fixed_object_dimensions:
+            raise ValueError('If present, fixed_object_positions argument needs to be of size num_objects*num_fixed_object_dimensions')
+        fixed_object_positions = np.reshape(fixed_object_positions, (num_objects, num_fixed_object_dimensions))
+    else:
+        fixed_object_positions = np.full((num_objects, num_fixed_object_dimensions), np.nan)
+    room_corners = np.asarray(room_corners)
+    if room_corners.shape != (2, num_moving_object_dimensions):
+        raise ValueError('room_corners argument must be of shape (2, num_moving_object_dimensions)')
+    num_dimensions = num_moving_object_dimensions + num_fixed_object_dimensions
+    anchor_positions = np.asarray(anchor_positions)
+    if anchor_positions.shape != (num_anchors, num_dimensions):
+        raise ValueError('anchor_positions argument must be of shape (num_anchors, num_moving_object_dimensions + num_fixed_object_dimensions)')
     parameter_structure = {
-        'room_corners': {
-            'shape': [2, num_dimensions],
-            'type': 'float32'
-        },
         'num_objects': {
             'shape': [],
             'type': 'int32'
+        },
+        'num_anchors': {
+            'shape': [],
+            'type': 'int32'
+        },
+        'num_moving_object_dimensions': {
+            'shape': [],
+            'type': 'int32'
+        },
+        'num_fixed_object_dimensions': {
+            'shape': [],
+            'type': 'int32'
+        },
+        'fixed_object_positions': {
+            'shape': [num_objects, num_fixed_object_dimensions],
+            'type': 'float32'
+        },
+        'room_corners': {
+            'shape': [2, num_moving_object_dimensions],
+            'type': 'float32'
+        },
+        'anchor_positions': {
+            'shape': [num_anchors, num_dimensions],
+            'type': 'float32'
         },
         'reference_time_interval': {
             'shape': [],
@@ -37,10 +76,6 @@ def localization_model(
         },
         'reference_drift': {
             'shape': [],
-            'type': 'float32'
-        },
-        'anchor_positions': {
-            'shape': [num_anchors, num_dimensions],
             'type': 'float32'
         },
         'reference_distance': {
@@ -62,8 +97,8 @@ def localization_model(
     }
 
     state_structure = {
-        'positions': {
-            'shape': [num_objects, num_dimensions],
+        'moving_object_positions': {
+            'shape': [num_objects, num_moving_object_dimensions],
             'type': 'float32'
         }
     }
@@ -76,12 +111,12 @@ def localization_model(
     }
 
     state_summary_structure = {
-        'positions_mean': {
-            'shape': [num_objects, num_dimensions],
+        'moving_object_positions_mean': {
+            'shape': [num_objects, num_moving_object_dimensions],
             'type': 'float32'
         },
-        'positions_sd': {
-            'shape': [num_objects, num_dimensions],
+        'moving_object_positions_sd': {
+            'shape': [num_objects, num_moving_object_dimensions],
             'type': 'float32'
         },
         'num_resample_indices': {
@@ -92,12 +127,15 @@ def localization_model(
 
     def parameter_model_sample():
         parameters = {
-            'room_corners': tf.constant(room_corners, dtype=tf.float32),
             'num_objects': tf.constant(num_objects, dtype=tf.int32),
+            'num_anchors': tf.constant(num_anchors, dtype = tf.int32),
+            'num_moving_object_dimensions': tf.constant(num_moving_object_dimensions, dtype = tf.int32),
+            'num_fixed_object_dimensions': tf.constant(num_fixed_object_dimensions, dtype = tf.int32),
+            'fixed_object_positions': tf.constant(fixed_object_positions, dtype=tf.float32),
+            'room_corners': tf.constant(room_corners, dtype=tf.float32),
+            'anchor_positions': tf.constant(anchor_positions, dtype = tf.float32),
             'reference_time_interval': tf.constant(reference_time_interval, dtype=tf.float32),
             'reference_drift': tf.constant(reference_drift, dtype=tf.float32),
-            'num_anchors': tf.constant(num_anchors, dtype = tf.int32),
-            'anchor_positions': tf.constant(anchor_positions, dtype = tf.float32),
             'reference_distance': tf.constant(reference_distance, dtype=tf.float32),
             'reference_mean_rssi': tf.constant(reference_mean_rssi, dtype=tf.float32),
             'mean_rssi_slope': tf.constant(mean_rssi_slope, dtype=tf.float32),
@@ -113,40 +151,61 @@ def localization_model(
             low = room_corners[0],
             high= room_corners[1]
         )
-        initial_positions = room_distribution.sample((num_samples, num_objects))
+        initial_moving_object_positions = room_distribution.sample((num_samples, num_objects))
         initial_state = {
-            'positions': initial_positions
+            'moving_object_positions': initial_moving_object_positions
         }
         return(initial_state)
 
     def transition_model_sample(current_state, current_time, next_time, parameters):
-        current_positions = current_state['positions']
+        current_moving_object_positions = current_state['moving_object_positions']
         reference_time_interval = parameters['reference_time_interval']
         reference_drift = parameters['reference_drift']
         room_corners = parameters['room_corners']
         time_difference = tf.cast(next_time - current_time, dtype=tf.float32)
         drift = reference_drift*tf.sqrt(time_difference/reference_time_interval)
         drift_distribution = tfp.distributions.TruncatedNormal(
-            loc = current_positions,
+            loc = current_moving_object_positions,
             scale = drift,
             low = room_corners[0],
             high= room_corners[1]
         )
-        next_positions = drift_distribution.sample()
+        next_moving_object_positions = drift_distribution.sample()
         next_state = {
-            'positions': next_positions
+            'moving_object_positions': next_moving_object_positions
         }
         return(next_state)
 
+    def object_positions_no_fixed_dimensions(state, parameters):
+        moving_object_positions = state['moving_object_positions']
+        object_positions = moving_object_positions
+        return object_positions
+
+    def object_positions_with_fixed_dimensions(state, parameters):
+        moving_object_positions = state['moving_object_positions']
+        fixed_object_positions = parameters['fixed_object_positions']
+        num_samples = tf.shape(moving_object_positions)[0]
+        fixed_object_positions_reshaped = tf.reshape(fixed_object_positions, (num_objects, num_fixed_object_dimensions))
+        fixed_object_positions_expanded = tf.expand_dims(fixed_object_positions_reshaped, axis = 0)
+        fixed_object_positions_repeated = tf.tile(fixed_object_positions_expanded, (num_samples, 1, 1))
+        object_positions = tf.concat((moving_object_positions, fixed_object_positions_repeated), axis = -1)
+        return object_positions
+
+    if num_fixed_object_dimensions == 0:
+        object_positions_function = object_positions_no_fixed_dimensions
+    else:
+        object_positions_function = object_positions_with_fixed_dimensions
+
     def rssi_distribution_function(state, parameters):
-        positions = state['positions']
+        # moving_object_positions = state['moving_object_positions']
         anchor_positions = parameters['anchor_positions']
         reference_distance = parameters['reference_distance']
         reference_mean_rssi = parameters['reference_mean_rssi']
         mean_rssi_slope = parameters['mean_rssi_slope']
         rssi_std_dev = parameters['rssi_std_dev']
+        object_positions = object_positions_function(state, parameters)
         relative_positions = tf.subtract(
-            tf.expand_dims(positions, axis = 1),
+            tf.expand_dims(object_positions, axis = 1),
             tf.expand_dims(tf.expand_dims(anchor_positions, 0), axis = 2))
         distances = tf.norm(relative_positions, axis = -1)
         log10_distances = tf.log(distances)/tf.log(10.0)
@@ -220,22 +279,22 @@ def localization_model(
         return(log_pdf)
 
     def state_summary(state, log_weights, resample_indices, parameters):
-        positions = state['positions']
-        positions_squared = tf.square(positions)
+        moving_object_positions = state['moving_object_positions']
+        moving_object_positions_squared = tf.square(moving_object_positions)
         weights = tf.exp(log_weights)
         weights_sum = tf.reduce_sum(weights)
-        positions_mean = tf.tensordot(weights, positions, 1)/weights_sum
-        positions_squared_mean = tf.tensordot(weights, positions_squared, 1)/weights_sum
-        positions_var = positions_squared_mean - tf.square(positions_mean)
-        positions_sd = tf.sqrt(positions_var)
+        moving_object_positions_mean = tf.tensordot(weights, moving_object_positions, 1)/weights_sum
+        moving_object_positions_squared_mean = tf.tensordot(weights, moving_object_positions_squared, 1)/weights_sum
+        moving_object_positions_var = moving_object_positions_squared_mean - tf.square(moving_object_positions_mean)
+        moving_object_positions_sd = tf.sqrt(moving_object_positions_var)
         unique_resample_indices, _ = tf.unique(resample_indices)
         num_resample_indices = tf.size(unique_resample_indices)
-        positions_mean_expanded = tf.expand_dims(positions_mean, 0)
-        positions_sd_expanded = tf.expand_dims(positions_sd, 0)
+        moving_object_positions_mean_expanded = tf.expand_dims(moving_object_positions_mean, 0)
+        moving_object_positions_sd_expanded = tf.expand_dims(moving_object_positions_sd, 0)
         num_resample_indices_expanded = tf.expand_dims(num_resample_indices, 0)
         state_summary = {
-            'positions_mean': positions_mean_expanded,
-            'positions_sd': positions_sd_expanded,
+            'moving_object_positions_mean': moving_object_positions_mean_expanded,
+            'moving_object_positions_sd': moving_object_positions_sd_expanded,
             'num_resample_indices': num_resample_indices_expanded
         }
         return state_summary
--- task_list.md
+++ task_list.md
@@ -1,9 +1,7 @@
 # Task list
 
-* Don't repeat code for two versions of observation_model
-* Add probability of missing measurements to simulation (including one at a time)
-* Add possibility of fixed dimensions
-* Add type and shape checking
+* Add data processing
+* Add visualization
 * Add logging
 * Add docstrings
 * Generate documentation
